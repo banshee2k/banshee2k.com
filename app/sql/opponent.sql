8e9eb86b8cbdd649425f05d6627d90b6237b3623
jdkato <joseph@jdkato.io>
2022-12-23 15:56:58 -0600

    Fix 'Team Stats'

--- app/sql/opponent.sql
+++ app/sql/opponent.sql
@@ -1,18 +1,18 @@
-SELECT
-       team.name,
-       Count(team.NAME) / 5                   AS gp,
-       Avg(pts)                               AS pts,
-       Avg(ast)                               AS ast,
-       Avg(reb)                               AS reb,
-       Avg("3pm")                             AS "3pm",
-       Avg("3pa")                             AS "3pa",
-       Sum("3pm") / Cast(Sum("3pa") AS FLOAT) AS "3p%",
-       Avg("fgm")                             AS "fgm",
-       Avg("fga")                             AS "fga",
-       Sum("fgm") / Cast(Sum("fga") AS FLOAT) AS "fg%",
-       Avg(stats.to)                          AS tov,
-       Avg(blk)                               AS blk,
-       Avg(stl)                               AS stl
+SELECT team.NAME,
+       team.abbr                                AS tid,
+       Count(team.NAME) / 5                     AS gp,
+       Sum(pts) / ( Count(team.NAME) / 5 )      AS pts,
+       Sum(ast) / ( Count(team.NAME) / 5 )      AS ast,
+       Sum(reb) / ( Count(team.NAME) / 5 )      AS reb,
+       Sum("3pm") / ( Count(team.NAME) / 5 )    AS "3pm",
+       Sum("3pa") / ( Count(team.NAME) / 5 )    AS "3pa",
+       Sum("3pm") / Cast(Sum("3pa") AS FLOAT)   AS "3p%",
+       Sum("fgm") / ( Count(team.NAME) / 5 )    AS "fgm",
+       Sum("fga") / ( Count(team.NAME) / 5 )    AS "fga",
+       Sum("fgm") / Cast(Sum("fga") AS FLOAT)   AS "fg%",
+       Avg(stats.to) / ( Count(team.NAME) / 5 ) AS tov,
+       Avg(blk) / ( Count(team.NAME) / 5 )      AS blk,
+       Avg(stl) / ( Count(team.NAME) / 5 )      AS stl
 FROM   stats
        JOIN player
          ON stats.player = player.id
